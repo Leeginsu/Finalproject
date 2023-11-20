15e34359f568c1eeb0158552607110c980e915a3
mjho0225 <mjho0225@gamil.com>
2023-11-20 15:22:31 +0900

    수정

--- Assets/01.Scripts/MJH/Final.cs
+++ Assets/01.Scripts/MJH/Final.cs
@@ -2,24 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using UnityEngine.UI;
 
 public class Final : MonoBehaviourPunCallbacks
 {
-    public GameObject cCount;
+
+    public GameObject correctCountUI;
+    public GameObject placeMentUI;
+
+    //public GameObject cCount;
     int correctCount; // 퍼즐 맞춘 개수
+    Transform[] placePos; // 배치 장소
+    public GameObject correctPic1;
+    public GameObject correctPic2;
+    public GameObject correctPic3;
+
+    public Text correctCountTxt;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (PhotonNetwork.IsMasterClient == true)
-        {
-            DontDestroyOnLoad(this.gameObject);
-        }
+        correctCount = 3;
+        correctCountTxt.text = correctCount + "개 중" + correctCount + "개 완성 !";
+        //if (PhotonNetwork.IsMasterClient == true)
+        //{
+            
+        //    DontDestroyOnLoad(this.gameObject);
+        //}
     }
 
     // Update is called once per frame
     void Update()
     {
         //correctCount = cCount.GetComponent<PreGameManager>().scoreCount;
+    }
+
+    void BtnEnter()
+    {
+        correctCountUI.SetActive(false);
+        placeMentUI.SetActive(true);
+    }
+
+    void BtnPlacement()
+    {
+        print("배치되었습니다.");
+
+        //PhotonNetwork.Instantiate("",placePos[0].position,Quaternion.identity);
+        correctPic1.SetActive(true);
+        correctPic2.SetActive(true);
+        correctPic3.SetActive(true);
+
     }
 }
