af59acd11202a4e6473d9de6c23fb4119cf64d4d
Leeginsu <135006635+Leeginsu@users.noreply.github.com>
2023-11-22 10:15:31 +0900

    로고씬 마무리

--- Assets/01.Scripts/KJJ/Title.cs
+++ Assets/01.Scripts/KJJ/Title.cs
@@ -5,13 +5,17 @@ using DG.Tweening;
 
 public class Title : MonoBehaviour
 {
+    public GameObject startButton;
     // Start is called before the first frame update
     void Start()
     {
         var cursor = DOTween.Sequence();
 
-        cursor.Append(transform.DOScale(1.3f, 1.5f));
-        cursor.Append(transform.DOScale(1f, 0.5f));
+        cursor.Append(transform.DOScale(1.3f, 1.5f))
+        .Join(transform.DORotate(new Vector3(0, 0, 1080), 1.5f, RotateMode.FastBeyond360))
+        //cursor.Join(transform.DORotate(new Vector3(0, 360, 0), 2));
+        .Append(transform.DOScale(1f, 0.5f))
+        .Append(startButton.transform.DOScale(1f, 1f));
     }
 
     // Update is called once per frame
